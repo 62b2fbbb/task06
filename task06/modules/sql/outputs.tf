da797d6203ff3912db08e02c4263d2c15ecf5da9
Zakhar Saprun <62b2fbbb@gmail.com>
2026-01-07 01:46:37 +0200

    finalV

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,8 +1,10 @@
 output "sql_connection_string" {
-  value     = "Server=tcp:${azurerm_mssql_server.srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_key_vault_secret.sql_login.value};Password=${azurerm_key_vault_secret.sql_pass.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-  sensitive = true
+  value       = "Server=tcp:${azurerm_mssql_server.srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${azurerm_key_vault_secret.sql_login.value};Password=${azurerm_key_vault_secret.sql_pass.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  sensitive   = true
+  description = "The connection string for the Azure SQL Database (ADO.NET format)."
 }
 
 output "sql_server_fqdn" {
-  value = azurerm_mssql_server.srv.fully_qualified_domain_name
+  value       = azurerm_mssql_server.srv.fully_qualified_domain_name
+  description = "The Fully Qualified Domain Name (FQDN) of the Azure SQL Server."
 }
--- task06/modules/webapp/outputs.tf
+++ task06/modules/webapp/outputs.tf
@@ -1,3 +1,4 @@
 output "app_hostname" {
-  value = azurerm_linux_web_app.linuxwebapp.default_hostname
+  value       = azurerm_linux_web_app.linuxwebapp.default_hostname
+  description = "The default hostname of the created Linux Web Application."
 }
