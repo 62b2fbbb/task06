892811756077ce424c08632f239350fa2df00040
Zakhar Saprun <62b2fbbb@gmail.com>
2026-01-07 02:37:24 +0200

    help me pls

--- task06/outputs.tf
+++ task06/outputs.tf
@@ -1,7 +1,11 @@
 output "sql_server_fqdn" {
-  value = module.SQLserver.sql_server_fqdn
+  value       = module.SQLserver.sql_server_fqdn
+  description = "The Fully Qualified Domain Name (FQDN) of the Azure SQL Server."
+
 }
 
 output "app_hostname" {
-  value = module.webapp.app_hostname
+  value       = module.webapp.app_hostname
+  description = "The default hostname of the created Linux Web Application."
+
 }
