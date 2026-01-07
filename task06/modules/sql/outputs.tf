7a5e3e1bb7bc005da353adeff8556e63a7af3032
Zakhar Saprun <62b2fbbb@gmail.com>
2026-01-07 22:43:04 +0200

    idk

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -10,9 +10,8 @@ output "sql_admin_password_secret_id" {
 
 output "sql_connection_string" {
   description = "Connection string for SQL Database"
-  value       = nonsensitive("Server=tcp:${azurerm_mssql_server.srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=admuser;Password=${random_password.pass.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-
-  sensitive = false
+  value       = "Server=tcp:${azurerm_mssql_server.srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=admuser;Password=${random_password.pass.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  sensitive   = false
 }
 
 output "sql_server_fqdn" {
