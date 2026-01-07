76e764ff0dc6a0937c1d49385f3d2c344dadf3bf
Zakhar Saprun <62b2fbbb@gmail.com>
2026-01-07 02:07:07 +0200

    finalVVV

--- task06/main.tf
+++ task06/main.tf
@@ -1,14 +1,13 @@
-resource "azurerm_resource_group" "rg" {
-  name     = local.rg_name
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name = local.rg_name
 }
 
 
 module "webapp" {
   source = "./modules/webapp"
 
-  rg_name               = azurerm_resource_group.rg.name
-  rg_loc                = azurerm_resource_group.rg.location
+  rg_name               = data.azurerm_resource_group.rg.name
+  rg_loc                = data.azurerm_resource_group.rg.location
   asp_name              = local.asp_name
   asp_sku               = var.asp_sku
   app_name              = local.app_name
@@ -19,8 +18,8 @@ module "webapp" {
 module "SQLserver" {
   source                    = "./modules/sql"
   name                      = local.sql_server_name
-  rg_name                   = azurerm_resource_group.rg.name
-  rg_loc                    = azurerm_resource_group.rg.location
+  rg_name                   = data.azurerm_resource_group.rg.name
+  rg_loc                    = data.azurerm_resource_group.rg.location
   sql_admin_secret_name     = var.sql_admin_secret_name
   sql_admin_secret_password = var.sql_admin_secret_password
   sql_server_name           = local.sql_server_name
